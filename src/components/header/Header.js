8d29789356404552ea2fd2a2ca9a3a093e7e3376
Oumaima <oumaimabk168@gmail.com>
2021-12-03 20:59:23 +0100

    fixing the header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,72 +1,13 @@
-import '../css/animate.css';
+import React from 'react'
+import './header.css';
 export default function Header() {
     return (
-        <section class="ftco-section">
-        <div class="container">
-            <div class="row">
-                
-                <div class="col-md-12">
-                    <div class="featured-carousel owl-carousel">
-                        <div class="item">
-                            <div class="row justify-content-center">
-                                <div class="col-md-11">
-                                    <div class="testimony-wrap d-md-flex">
-                                        <div class="img" style={{ backgroundImage:"url(./assets/img/c1.jpg)" }}></div>
-                                        <div class="text text-center p-4 py-xl-5 px-xl-5 d-flex align-items-center">
-                                            <div class="desc w-100">
-                                                <p class="h3 mb-5">"Incredible services and amazing customer support</p>
-                                                <div class="pt-4">
-                                                    <p class="name mb-0">&mdash; Joy Smith</p>
-                                                    <span class="position">Project Manager</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <div class="row justify-content-center">
-                                <div class="col-md-11">
-                                    <div class="testimony-wrap d-md-flex">
-                                        <div class="img" style={{backgroundImage:"url(./assets/img/c2.jpg)" }}></div>
-                                        <div class="text text-center p-4 py-xl-5 px-xl-5 d-flex align-items-center">
-                                            <div class="desc w-100">
-                                                <p class="h3 mb-5">"Incredible services and amazing customer support</p>
-                                                <div class="pt-4">
-                                                    <p class="name mb-0">&mdash; Rony Smith</p>
-                                                    <span class="position">Project Manager</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <div class="row justify-content-center">
-                                <div class="col-md-11">
-                                    <div class="testimony-wrap d-md-flex">
-                                        <div class="img" style={{backgroundImage:"url(./assets/img/c3.jpg)" }}></div>
-                                        <div class="text text-center p-4 py-xl-5 px-xl-5 d-flex align-items-center">
-                                            <div class="desc w-100">
-                                                <p class="h3 mb-5">"Incredible services and amazing customer support</p>
-                                                <div class="pt-4">
-                                                    <p class="name mb-0">&mdash; John Doe</p>
-                                                    <span class="position">Project Manager</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <div className="header">
+            <div className="headerTitles">
+            <div className="headerTitleLg">Create a unique, stunning blog</div>
+                <div className="headerTitleSm"> Nothing could be simpler and it's free</div>
             </div>
+            <img className="headerImg" src="./assets/img/c5.jpg" />
         </div>
-    </section>
-       
     )
 }
-
