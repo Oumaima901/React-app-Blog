7b7aae82a2e6eab8cfc4555fb8af70dce53e4d29
Oumaima <oumaimabk168@gmail.com>
2021-12-05 21:45:31 +0100

    adding login and register page with the css style

---
+++ src/pages/login/Login.js
@@ -0,0 +1,21 @@
+import './login.css';
+
+export default function Login() {
+    return (
+        <div className="login">
+           
+           
+            <form  className="loginform">
+            <span className="loginTitle">Sign In</span>
+                <label>Email</label>
+                <input type="text" className="loginInput" placeholder="Enter your email..."/>
+                <label>Password</label>
+                <input type="password" className="loginInput"  placeholder="Enter your password..."/>
+                <button className="loginButton">Login</button>
+                <button className="LoginRegisterButton">Register</button>
+            </form>
+           
+            <img  alt="" className="imgform" src="./assets/img/c6.jpg"/>
+        </div>
+    )
+}
---
+++ src/pages/register/Register.js
@@ -0,0 +1,24 @@
+import './register.css';
+
+export default function Register() {
+    return (
+        <div className="register">
+           
+           
+            <form  className="registerform">
+            <span className="registerTitle">Sign up</span>
+            <label>Username</label>
+                <input type="text" className="registerInput" placeholder="Enter your username..."/>
+                <label>Email</label>
+                <input type="text" className="registerInput" placeholder="Enter your email..."/>
+                <label>Password</label>
+                <input type="password" className="registerInput"  placeholder="Enter your password..."/>
+                <button className="registerButton">Register</button>
+                <button className="registerLoginButton">Login</button>
+            </form>
+           
+            <img  alt="" className="imgform" src="./assets/img/c7.png"/>
+        </div>
+   
+    )
+}
